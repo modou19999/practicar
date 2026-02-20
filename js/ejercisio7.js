961ba98b962cb797d7907a6d5438d77010de6705
Modou fall <serignemodoufall1@gmail.com>
2026-02-20 13:39:39 +0100

    Update

--- js/ejercisio7.js
+++ js/ejercisio7.js
@@ -1,4 +1,15 @@
 // DOM
 
-const element = document.getElementById("text");
-Element.textContent = "hola estoy practicando java script";
+// 1. Obtener el elemento por su id
+const element = document.getElementById("texto");
+
+// 2. Cambiar el contenido del párrafo
+element.textContent = "hola estoy practicando java script";
+
+// 1. Obtener el botón por su id
+const button = document.getElementById("miButton");
+
+// 2. Agregar un evento de click
+button.addEventListener("click", function () {
+  console.log("¡Has hecho clic en el botón!");
+});
